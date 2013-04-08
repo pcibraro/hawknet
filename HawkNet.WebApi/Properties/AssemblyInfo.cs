8052bdb43c8456d393b3d0991ffbddab92c8c2b4
pcibraro <pcibraro@hotmail.com>
2013-04-08 15:34:15 -0300

    Updated version and reference from nuget

--- HawkNet.WebApi/Properties/AssemblyInfo.cs
+++ HawkNet.WebApi/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyFileVersion("1.2.0.3")]
 
 #if SIGNED
 [assembly: AssemblyKeyFileAttribute("..\\hawk.snk")]
