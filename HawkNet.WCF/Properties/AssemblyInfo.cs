d2f05c34f55be731225c7181e0a12810c73d2c92
pcibraro <pcibraro@hotmail.com>
2013-04-11 17:20:30 -0300

    Updated nuget package name

--- HawkNet.WCF/Properties/AssemblyInfo.cs
+++ HawkNet.WCF/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hawk Authentication Protocol Implementation for .NET")]
+[assembly: AssemblyTitle("HawkNet.WCF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
