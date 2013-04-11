f92ddd7a20fbeddf763347efb519be284524716d
pcibraro <pcibraro@hotmail.com>
2013-04-11 16:44:17 -0300

    Added nuget spec and modified version

--- HawkNet.WCF/Properties/AssemblyInfo.cs
+++ HawkNet.WCF/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.3")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 #if SIGNED
 [assembly: AssemblyKeyFileAttribute("..\\hawk.snk")]
