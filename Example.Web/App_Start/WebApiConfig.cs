48db0b91f68ac2bb229bf24fccdda4cf6708bdd9
pcibraro <pcibraro@hotmail.com>
2014-01-31 14:12:52 -0300

    Added javascript web client

---
+++ Example.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using HawkNet.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using HawkNet;
+
+namespace Example.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Filters.Add(new RequiresHawkAttribute((id) =>
+                {
+                    return new HawkCredential
+                    {
+                        Id = "dh37fgj492je",
+                        Key = "werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn",
+                        Algorithm = "hmacsha256",
+                        User = "steve"
+                    };
+                }));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Example.Web/Controllers/Api/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Example.Web.Controllers.Api
+{
+    public class HelloWorldController : ApiController
+    {
+        public string Get()
+        {
+            return "hello " + this.User.Identity.Name;
+        }
+    }
+}
