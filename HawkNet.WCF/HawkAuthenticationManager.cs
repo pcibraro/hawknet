ca948243f28a0f5133d7e31deb3389d8ca8a30cd
dotbatman <dotbatman@nerdfortress.com>
2016-03-31 10:31:10 -0400

    Initial version of HawkServiceBehavior for applying WCF authentication/authorization providers

---
+++ HawkNet.WCF/HawkAuthenticationManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.ServiceModel.Channels;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Policy;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using System.ServiceModel.Web;
+using System.Security;
+using System.Web;
+using System.Linq;
+
+namespace HawkNet.WCF
+{
+    class HawkAuthenticationManager : ServiceAuthenticationManager
+    {
+        const string HawkScheme = "Hawk";
+
+        static TraceSource TraceSource = new TraceSource("HawkNet.WCF");
+
+        Func<string, HawkCredential> credentials;
+        int timeskewInSeconds;
+        string schemeOverride;
+
+        public HawkAuthenticationManager(Func<string, HawkCredential> credentials, int timeskewInSeconds, string schemeOverride)
+            : base()
+        {
+            this.credentials = credentials;
+            this.timeskewInSeconds = timeskewInSeconds;
+            this.schemeOverride = schemeOverride;
+
+            if (Trace.CorrelationManager.ActivityId == Guid.Empty)
+                Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+        }
+
+        public override ReadOnlyCollection<IAuthorizationPolicy> Authenticate(ReadOnlyCollection<IAuthorizationPolicy> authPolicy, Uri listenUri, ref Message requestMessage)
+        {
+            IPrincipal principal = ExtractCredentials(requestMessage);
+            var policies = new List<IAuthorizationPolicy>();
+            policies.Add(new HawkPrincipalAuthorizationPolicy());
+
+            if (principal != null)
+            {
+                requestMessage.Properties["Principal"] = principal;
+            }
+
+            return policies.AsReadOnly();
+        }
+
+        private IPrincipal ExtractCredentials(Message requestMessage)
+        {
+            var request = (HttpRequestMessageProperty)requestMessage.Properties[HttpRequestMessageProperty.Name];
+
+            var authHeader = request.Headers["Authorization"];
+            if (authHeader != null && authHeader.StartsWith(HawkScheme, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var hawk = authHeader.Substring(HawkScheme.Length).Trim();
+
+                TraceSource.TraceInformation(string.Format("{0} - Received Auth header: {1}",
+                    Trace.CorrelationManager.ActivityId, hawk));
+
+                var uri = new Uri(HttpUtility.UrlDecode(requestMessage.Properties.Via.AbsoluteUri));
+
+                var principal = Hawk.Authenticate(hawk,
+                    request.Headers["host"],
+                    request.Method,
+                    new UriBuilder (uri) { Scheme = (!string.IsNullOrEmpty(schemeOverride)) ? schemeOverride : uri.Scheme }.Uri,
+                    this.credentials,
+                    this.timeskewInSeconds);
+
+                return principal;
+            }
+            return null;
+        }
+    }
+}
---
+++ HawkNet.WCF/HawkAuthorizationManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.IdentityModel.Claims;
+using System.Net;
+
+namespace HawkNet.WCF
+{
+    class HawkAuthorizationManager : ServiceAuthorizationManager
+    {
+        public bool SendChallenge { get; set; }
+
+        public HawkAuthorizationManager(bool sendChallenge) : base()
+        {
+            SendChallenge = sendChallenge;
+        }
+
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            if (!operationContext.ServiceSecurityContext.AuthorizationContext.ClaimSets.Any())
+            {
+                if (SendChallenge)
+                {
+                    var newReply = Message.CreateMessage(MessageVersion.None, null);
+                    var responseProperty = new HttpResponseMessageProperty() { StatusCode = HttpStatusCode.Unauthorized };
+
+                    var ts = Hawk.ConvertToUnixTimestamp(DateTime.Now).ToString();
+                    var challenge = string.Format("ts=\"{0}\" ntp=\"{1}\"",
+                        ts, "pool.ntp.org");
+
+                    responseProperty.Headers.Add("WWW-Authenticate", challenge);
+
+                    newReply.Properties[HttpResponseMessageProperty.Name] = responseProperty;
+
+                    //overwrite the original reply with the unauthorized message.
+                    operationContext.RequestContext.Reply(newReply);
+                }
+                return false;
+            }
+
+            return base.CheckAccessCore(operationContext);
+        }
+    }
+}
